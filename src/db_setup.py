10b6e174d037f5d79435555b06b0350eadd2dc35
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-15 19:57:52 -0700

    fb chatbot

--- src/db_setup.py
+++ src/db_setup.py
@@ -8,7 +8,7 @@ guest = User(username='guest')
 admin_address = Address(full_address='1 Market St, San Francisco CA 94105',
                         user=admin)
 guest_address = Address(
-    full_address='1 Telegraph Hill Blvd, San Francisco, CA 94133',
+    full_address='160 Spear St., San Francisco, CA 94133',
     user=guest)
 db.session.add(admin)  # This will also add admin_address to the session.
 db.session.add(guest)  # This will also add guest_address to the session.
